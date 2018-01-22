e1c44d8a3967ff54c047967cb3accba9a667fd43
Michael Koontz <mikoontz@gmail.com>
2018-01-22 17:53:39 +0000

    new live script for week 3 and pushing all week 2 changes to GitHub

--- scripts/2018-R-DAVIS-w02.R
+++ scripts/2018-R-DAVIS-w02.R
@@ -3,3 +3,47 @@
 # first part of class, overview GIT
 
 # using git status
+
+# A test! Mike is typing now.
+
+# Simplest thing we can do is write math
+1 + 1
+2e3
+
+# Functions!
+# Think of functions with their 3-part summary
+# What arguments do they take in?
+# What do they do to those arguments?
+# What do they return?
+
+log(1000)
+
+# Nested functions
+log(exp(3))
+
+?log
+
+log10(1000)
+
+log(1000)
+# is the same as
+log(x = 1000)
+# is the same as
+log(x = 1000, base = exp(1))
+# is the same as (different order, but named arguments)
+log(base = exp(1), x = 1000)
+# is NOT the same as
+log(exp(1), 1000)
+
+# Assignment operator is <-
+a <- 1000
+# In order to retrieve the value stored in an R object,
+# run a line of code with just the object name
+a
+
+# Variables can be reassigned
+a <- 30
+a
+
+
+
