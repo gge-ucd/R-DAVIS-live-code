f62f2fbdcb13ffccd9d127a42412626205894ad6
ryanpeek <rapeek@ucdavis.edu>
2018-02-13 16:03:32 -0800

    update with w6, add surveys_complete.csv

--- scripts/2018-R-DAVIS-w06.R
+++ scripts/2018-R-DAVIS-w06.R
@@ -1,2 +1,190 @@
 # R-DAVIS 2018
 # Week 06
+
+# First step is to load the packages that we need using the library() function
+library(tidyverse)
+
+# Next step is to get the surveys_complete data
+surveys_complete <- read_csv("data_output/surveys_complete.csv")
+glimpse(surveys_complete)
+
+# Let's see what happens with the code given to us
+ggplot(data = surveys_complete, aes(x = weight, y = hindfoot_length)) +
+  geom_point()
+
+# How would we make a scatterplot of weight versus species ID
+# I accidentally made a misnamed variable so I remove it here using the rm() function.
+rm(surveys_compete)
+
+ggplot(data = surveys_complete, aes(x = species_id, y = weight)) +
+         geom_point()
+
+# We said this isn't a great way to display the data because it's ugly. And it is hard to *see* the data.
+
+ggplot(data = surveys_complete, aes(x = species_id, y = weight)) +
+  geom_boxplot()
+
+# how might we order the box plots descending by weight?
+
+ggplot(data = surveys_complete, aes(x = reorder(species_id, -weight), y = weight)) + geom_boxplot()
+
+# how about ascending by weight?
+ggplot(data = surveys_complete, aes(x = reorder(species_id, weight), y = weight)) + geom_boxplot()
+
+# How can we make a horizontally oriented boxplot? Typically the y aesthetic is the continuous variable and the x has to be the categorical variable
+ggplot(data = surveys_complete, aes(x = weight, y = species_id)) +
+  geom_boxplot()
+
+# Here we use an additional layer that "flips" the coordinates: coord_flip()
+ggplot(data = surveys_complete, aes(x = reorder(species_id, weight), y = weight)) + geom_boxplot() +
+  coord_flip()
+
+# back to the original
+ggplot(data = surveys_complete, aes(x = species_id, y = weight)) +
+  geom_boxplot()
+
+# Let's add the data back in
+ggplot(data = surveys_complete, aes(x = species_id, y = weight)) +
+  geom_boxplot() +
+  geom_jitter(alpha = 0.3, color = "tomato")
+
+# We lost the boxplots!
+ggplot(data = surveys_complete, aes(x = species_id, y = weight)) +
+  geom_jitter(alpha = 0.3, color = "tomato") +
+  geom_boxplot()
+
+# A challenge!
+# geom_boxplot adds a boxplot, but hides the shape of the data. Try adding a violin geometry using the same data.
+
+ggplot(data = surveys_complete, aes(x = species_id, y = weight)) +
+  geom_violin()
+
+
+# Modify the y-axis to be on the log10 scale
+ggplot(data = surveys_complete, aes(x = species_id, y = weight)) +
+  geom_violin() +
+  scale_y_log10()
+
+# Careful adding points back in when you have scaled your data because you may lose some resolution
+ # geom_jitter()
+
+# What if we want a custom scale? Like log to the base 2?
+ggplot(data = surveys_complete, aes(x = species_id, y = log(weight, base = 2))) +
+  geom_violin()
+
+# Let's plot the number of counts per year for each species.
+# For each species/year combination, count the number of observations.
+yearly_counts <- surveys_complete %>% 
+  group_by(year, species_id) %>% 
+  tally()
+
+yearly_counts
+
+ggplot(data = yearly_counts, aes(x = year, y = n)) +
+  geom_line()
+
+# We want a separate line for each species
+ggplot(data = yearly_counts, aes(x = year, y = n)) +
+  geom_line(aes(group = species_id))
+
+# Let's use the color aesthetic instead of group
+
+ggplot(data = yearly_counts, aes(x = year, y = n)) +
+  geom_line(aes(color = species_id))
+
+# Faceting
+# Multiple sub panels on a single plot. Use a factor as your data to facet on
+ggplot(data = yearly_counts, aes(x = year, y = n)) +
+  geom_line() +
+  facet_wrap(~ species_id)
+
+# Let's just look at our favorite 4 species
+yearly_counts %>% 
+  filter(species_id == "DM" | species_id == "DO" | species_id == "OT" | species_id == "RM") %>% 
+  ggplot(aes(x = year, y = n)) +
+  geom_line() +
+  facet_wrap( ~ species_id)
+
+yearly_sex_counts <- surveys_complete %>% 
+  group_by(year, species_id, sex) %>% 
+  tally()
+
+yearly_sex_counts
+
+ggplot(data = yearly_sex_counts, aes(x = year, y = n, color = sex)) +
+  geom_line() +
+  facet_wrap( ~ species_id)
+
+# Modify the overall look of a plot using themes
+ggplot(data = yearly_sex_counts, aes(x = year, y = n, color = sex)) +
+  geom_line() +
+  facet_wrap( ~ species_id) +
+  theme_bw()
+
+# How to save a plot
+# ggsave()
+
+# By default, ggsave() will save the last plot that you created
+
+ggsave(filename = "figures/my_plot.pdf")
+
+my_plot <- ggplot(data = yearly_sex_counts, aes(x = year, y = n)) +
+  geom_line(aes(color = species_id))
+
+my_plot
+
+ggsave(filename = "figures/abundance_by_year_by_species.pdf", plot = my_plot)
+
+# Challenge!
+# Create a plot depicting the change in average weight for each species through time. Then save that plot in your figures/ folder.
+
+# Hint: What do you need to do to the surveys_complete data frame to get the average weight per species per year?
+
+# Hint: What function does the "per" in the above hint suggest you should use on the surveys_complete dataframe?
+
+# Hint: To me, "per" suggests to use "group_by"
+
+# Hint: the mean() is a summary function-- it takes a vector of values and collapses it into a single value
+
+mn_wgt_surveys <- 
+  surveys_complete %>% 
+  group_by(species_id, year) %>% 
+  summarize(mn_wgt = mean(weight))
+
+# Change the line thickness
+mn_wgt_figure <- 
+  ggplot(data = mn_wgt_surveys, aes(x = year, y = mn_wgt)) +
+  geom_line(aes(color = species_id), size = 3)
+
+(mn_wgt_figure <- 
+  ggplot(data = mn_wgt_surveys, aes(x = year, y = mn_wgt)) +
+  geom_line(aes(color = species_id)) +
+  facet_grid(species_id ~ .))
+
+# Wrap an assignment in parentheses in order to also show in the console what is happening
+
+a <- 5
+(b <- 6)
+mn_wgt_figure
+
+ggsave(filename = "figures/mn_wgt_figure.pdf", plot = mn_wgt_figure)
+
+# the write. and read. functions
+
+# type write. and hit TAB
+# type read. and hit TAB
+
+# main differences between RDA and RDS files
+
+
+
+
+
+
+
+
+
+
+
+
+
